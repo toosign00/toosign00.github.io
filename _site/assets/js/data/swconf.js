dae07f85b50377e4220e707ad33cacfefbf3bf81
노현수 <kevinsoo1014@gmail.com>
2024-10-05 05:33:49 +0900

    meta 태그 안보이게 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728073414',resources: [
+    cacheName: 'chirpy-1728074009',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
