24be678b47f20a02e3d22d3ffac81c5ba8a7e246
노현수 <kevinsoo1014@gmail.com>
2024-10-05 02:58:22 +0900

    수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728064494',resources: [
+    cacheName: 'chirpy-1728064605',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
