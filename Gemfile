f28f3431249c305d8c19d4bb0c004557e92ebdbb
노현수 <kevinsoo1014@gmail.com>
2024-10-05 03:02:46 +0900

    수정

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@
 
 source "https://rubygems.org"
 
+
 gem "jekyll-theme-chirpy", "~> 7.1", ">= 7.1.1"
 
 gem "html-proofer", "~> 5.0", group: :test
--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728064605',resources: [
+    cacheName: 'chirpy-1728064951',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
