4416bae09db845b13c274bcbf79c6f1cb35e25ed
노현수 <kevinsoo1014@gmail.com>
2024-10-07 12:39:05 +0900

    줄 간격 조정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728271456',resources: [
+    cacheName: 'chirpy-1728271834',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
