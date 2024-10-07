3c5ad4c3b28b0100844ee6ea2e91f6e624bf9e6a
노현수 <kevinsoo1014@gmail.com>
2024-10-07 15:42:13 +0900

    즉시 서비스가 활성화 되도록 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728271834',resources: [
+    cacheName: 'chirpy-1728282776',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
