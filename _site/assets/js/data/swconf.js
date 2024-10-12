9a4417afd762d10fb63e86789e0852eecdfac631
노현수 <kevinsoo1014@gmail.com>
2024-10-12 23:35:16 +0900

    loading="eager" 속성 추가

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728742175',resources: [
+    cacheName: 'chirpy-1728743706',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
