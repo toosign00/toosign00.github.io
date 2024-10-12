e26dbb1dd219cdb29e9c865fc0460d294708ea51
노현수 <kevinsoo1014@gmail.com>
2024-10-12 23:55:48 +0900

    주석문 삭제

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728743706',resources: [
+    cacheName: 'chirpy-1728744706',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
