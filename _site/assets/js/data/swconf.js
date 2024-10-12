f93aaa4ac57fa0be1761824a4e8eac1f15beca98
노현수 <kevinsoo1014@gmail.com>
2024-10-13 00:37:48 +0900

    Refactor CSS in about.md, archives/index.html, categories/index.html, feed.xml, sitemap.xml, and tags/index.html

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728747317',resources: [
+    cacheName: 'chirpy-1728747448',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
