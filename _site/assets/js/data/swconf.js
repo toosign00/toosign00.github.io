be2d52541330eb18781c3defbe15670e19894c4f
toosign <kevinsoo1014@gmail.com>
2024-10-21 03:47:47 +0900

    readme 수정, 자소서 항목 작성, 항목 순서 변경

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728968529',resources: [
+    cacheName: 'chirpy-1729450050',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
