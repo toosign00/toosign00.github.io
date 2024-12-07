5d33f150595938a3714d6667e502218a49b2e441
toosign <kevinsoo1014@gmail.com>
2024-12-08 02:14:51 +0900

    config.example.js 파일 추가: 환경변수 값 템플릿 제공

---
+++ assets/js/config.example.js
@@ -0,0 +1,7 @@
+// config.example.js
+// 환경변수 값 템플릿
+const config = {
+  EMAILJS_PUBLIC_KEY: "YOUR_PUBLIC_KEY",
+  EMAILJS_SERVICE_ID: "YOUR_SERVICE_ID",
+  EMAILJS_TEMPLATE_ID: "YOUR_TEMPLATE_ID"
+};
