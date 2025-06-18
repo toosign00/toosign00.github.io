ebe41037bf44659c1e82321f2da2eb5d40195d6a
toosign <kevinsoo1014@gmail.com>
2025-06-18 22:09:34 +0900

    🔥 remove: 불필요한 파일 삭제

--- src/routes/lazyComponents.ts
+++
@@ -1,8 +0,0 @@
-import { lazy } from 'react';
-
-// 지연 로딩할 컴포넌트들
-export const LazyProjectPage = lazy(() =>
-  import('@/components/ProjectModal/components/ProjectPage').then((module) => ({
-    default: module.ProjectPage,
-  })),
-);
